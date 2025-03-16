0d1193d983ee5b88888ebf428026070c70a7aa5d
ChuqiaoHuang <chuqiao575@gmail.com>
2025-03-16 17:49:40 -0400

    update main

--- src/game_master/__main__.py
+++ src/game_master/__main__.py
@@ -4,8 +4,13 @@ directly from command line, as opposed to importing it into another program.
 """
 
 import argparse
-from .game_master import spin_the_bottle, countdown, random_multiple_people_punishment
-
+from .game_master import (
+    spin_the_bottle,
+    countdown,
+    random_multiple_people_punishment,
+    random_game_idea,
+    random_dare,
+)
 
 def main():
     """
@@ -16,27 +21,34 @@ def main():
     parser.add_argument('--bottle', nargs="+", help="Spin the bottle with a list of names.")
     parser.add_argument('--countdown', type=int, help="Countdown timer in second")
     parser.add_argument('--punish', nargs="+", help="Punish a random number of players from a list.")
-    
+    parser.add_argument("--game", type=int, help="Suggest a random game for 2, 4, 6, or 8 players.")
+    parser.add_argument("--dare", type=str, help="Generate a random dare based on the level of difficulty(easy, medium, hard).")
+    parser.add_argument("--help", action="store_true", help="Show available functions.")
+
 
     args = parser.parse_args()
 
     if args.bottle:
         print("Bottle is spinning..."+spin_the_bottle(args.bottle))
+
     elif args.countdown:
         print("Start counting...")
         result=countdown(args.countdown)
         print(f"Countdown finished at: {result}")
+
     elif args.punish:
         num = args.punish[0]
         names = args.push[1:]
         print(random_multiple_people_punishment(num, names))
+    
+    elif args.game:
+        print(random_game_idea(args.game))
+
+    elif args.dare:
+        print(random_dare(args.dare))
+
     else:
         print("Use --help to see available options.")
-
-        
-        
-        '''# line = game.get()  # get a line of text
-        print("Hi i am in main")  # print it out'''
 
 
 if __name__ == "__main__":
